46592efa38d4a742eae3960d24cb47f495bc66d5
sm5ajv <33346260+sm5ajv@users.noreply.github.com>
2018-04-04 21:06:00 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -9,37 +9,82 @@
       return $line_of_text;
   }
   
+  function printRawTable($rows) {
+    //P5/SM5COP/P,21001.0,SM5AJV,RTTY,2018-04-02,14:32,RBN,AJV-trigger
+    //SV8RV,J5MW,7030.2,CW,RBN,2018-04-02,15:26,
+    //0     1    2      3  4   5           6
+    $n = 0;
+    $max_lines = 20;
+    echo '<pre>';
+    printf("%-13s %-13s %10s  %-8s %-5s %-12s %-12s %-16s\n", 
+           "DE", "DX", "FREQ", "MODE", "UTC", "DATE", "SOURCE", "ENTITY");
+    printf("-----------------------------------------------------------------------------------------\n");
+    foreach (array_reverse($rows) as $row) {
+      if (strlen($row[0])> 0) {
+        printf("%-13s %-13s %10.1f  %-8s %-5s %-12s %-12s %-16s\n", 
+              $row[0], $row[1], $row[2], $row[3], $row[6], $row[5], $row[4], $row[7]);
+        $n++;
+        if ($n >= $max_lines) {
+          break;
+       }
+      }
+    }
+    echo '</pre>';          
+  }
+  
+  function printHtmlTable($rows) {
+    $n = 0;
+    $max_lines = 20; 
+    printf('<table>');
+    printf('<tr><th>DE</th> <th>DX</th> <th>FREQ</th> <th>MODE</th> 
+                <th>UTC</th> <th>DATE</th> <th>SOURCE</th> <th>ENTITY</th> </tr>');
+    foreach (array_reverse($rows) as $row) {
+      if (strlen($row[0])> 0) {
+        printf("<tr><td>%-13s</td> 
+                    <td><a href=https://www.qrz.com/db/%s>%-13s</a></td>
+                    <td>%10.1f</td>
+                    <td>%-8s</td>
+                    <td>%-5s</td>
+                    <td>%-12s</td>
+                    <td>%-12s</td>
+                    <td>%-16s</td></tr>\n", 
+              $row[0], $row[1], $row[1], $row[2], $row[3], $row[6], $row[5], $row[4], $row[7]);
+        
+        $n++;
+        if ($n >= $max_lines) {
+          break;
+       }
+      }
+    }
+  printf('</table>');    
+  }
+  
   echo '<!DOCTYPE html>';
   echo '<html>';
   echo '<head>';
   echo '<meta http-equiv="refresh" content="10">';
   echo '<title>Needed Spots</title>';
+  echo '<style>
+table, th, td {
+    border: 0px solid black;
+    border-collapse: collapse;
+    font-family: Arial, Verdana, sans-serif;
+    font-size: 12px;
+}
+th, td {
+    padding: 5px;
+}
+th {
+    text-align: left;
+    background-color: #d2d5db;
+}
+</style>';
   echo '</head>';
   echo '<body>';
   echo '<h2>NEEDED SPOTS FROM HAM-ALERT</h2>';
-  echo '<pre>';
-  //$content = file_get_contents('spots.txt');
   $rows = readCSV('spots.csv');
-  //P5/SM5COP/P,21001.0,SM5AJV,RTTY,2018-04-02,14:32,RBN,AJV-trigger
-  //SV8RV,J5MW,7030.2,CW,RBN,2018-04-02,15:26,
-  //0     1    2      3  4   5           6
-  printf("%-13s %-13s %10s  %-8s %-5s %-12s %-10s\n", 
-         "DE", "DX", "FREQ", "MODE", "UTC", "DATE", "SOURCE");
-  printf("----------------------------------------------------------------------------\n");
-  $n = 0;
-  $max_lines = 20;
-  foreach (array_reverse($rows) as $row) {
-    if (strlen($row[0])> 0) {
-      printf("%-13s %-13s %10.1f  %-8s %-5s %-12s %-10s\n", 
-            $row[0], $row[1], $row[2], $row[3], $row[6], $row[5], $row[4]);
-      $n++;
-      if ($n >= $max_lines) {
-        break;
-     }
-    }
-  }
-  echo $content;
-  echo '</pre>';
+  //printRawTable($rows);
+  printHtmlTable($rows);
   echo '</body>';
   echo '</html>';
        
